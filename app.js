99e673bf96dc12a8347e6cea29ff538bc18bb90c
shumphries11 <santitahumphries@gmail.com>
2022-01-15 17:01:22 -0600

    added break lines

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ for(let a = 0; a < friends.length; a++) {
         if(b == 2, b--) {
             console.log(b + " lines of code in the file, " + b + " lines of code; " + friends[a] +  " strikes one out, clears it all out, " + (b - 1) + " line of code in the file.");
         } 
-        
+
         if(b == 1, b--) {
             console.log(b + " line of code in the file, " + b + " line of code; " + friends[a] +  " strikes one out, clears it all out, " + (b - 1) + " lines of code in the file.");
         }
+        
     }
 
+console.log("------------------------");
+console.log("------------------------");
         
 }
