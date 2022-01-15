51208c00e6e87162d0c203a72d04220c65ba8828
shumphries11 <santitahumphries@gmail.com>
2022-01-15 16:54:17 -0600

    Part 1 done

---
+++ app.js
@@ -0,0 +1,17 @@
+let friends = ["James", "Tiffany", "Camilla", "Tristan", "Ariel"];
+
+for(let a = 0; a < friends.length; a++) {
+    for(let b = 99; b > 2; b--) {
+        console.log(b + " lines of code in the file, " + b + " lines of code; " + friends[a] +  " strikes one out, clears it all out, " + (b - 1) + " lines of code in the file.");
+        
+        if(b == 2, b--) {
+            console.log(b + " lines of code in the file, " + b + " lines of code; " + friends[a] +  " strikes one out, clears it all out, " + (b - 1) + " line of code in the file.");
+        } 
+        
+        if(b == 1, b--) {
+            console.log(b + " line of code in the file, " + b + " line of code; " + friends[a] +  " strikes one out, clears it all out, " + (b - 1) + " lines of code in the file.");
+        }
+    }
+
+        
+}
