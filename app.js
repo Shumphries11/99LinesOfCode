9a6e184f1e43d9cac1476ef6f2c139555cb270c0
shumphries11 <santitahumphries@gmail.com>
2022-01-27 15:34:20 -0600

    fixed name header and made it uppercase

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let friends = ["James", "Tiffany", "Camilla", "Tristan", "Ariel"];
 
 for(let a = 0; a < friends.length; a++) {
+    console.log(friends[a].toUpperCase() + ":")
     for(let b = 99; b > 2; b--) {
         console.log(b + " lines of code in the file, " + b + " lines of code; " + friends[a] +  " strikes one out, clears it all out, " + (b - 1) + " lines of code in the file.");
         
